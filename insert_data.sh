5da4e7af52bc76317404d22bf364ab5ca30469a2
Juan Alzate <jupawe3092@gmail.com>
2024-04-18 19:00:59 +0000

    Save progress: 610.1

--- insert_data.sh
+++ insert_data.sh
@@ -4,7 +4,7 @@
 
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 
-cat courses.csv | while IFS="," read MAJOR COURSE
+cat courses_test.csv | while IFS="," read MAJOR COURSE
 
 do
   # get major_id
