8a75cd88369d9cf243b16663de0cfa8f1359f0a2
Juan Alzate <jupawe3092@gmail.com>
2024-04-22 21:29:28 +0000

    Save progress: 850.1

--- insert_data.sh
+++ insert_data.sh
@@ -29,13 +29,14 @@ do
   # get course_id
   COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
   
-
   # if not found
-
+  if [[ -z $COURSE_ID ]]
+  then 
   # insert course
 
-  # get new course_id
 
+  # get new course_id
+  fi
   # insert into majors_courses
   fi
 done
